8a5ac06c5e4ece6559d662ba3c0442019e338454
Andy Cheng <andy@95539500.com>
2022-05-24 10:56:42 +0100

    added meta data

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 const fs = require("fs");
 const path = require("path");
-const AvatarGenerator = require('avatar-generator')
-const avatar = new AvatarGenerator({
-  //All settings are optional.
-  parts: ['background', 'face', 'clothes', 'head', 'hair', 'eye', 'mouth'], //order in which sprites should be combined
-  partsLocation: path.join(__dirname, '/img'), // path to sprites
-  imageExtension: '.png' // sprite file extension
-})
+
 const generateData= async (i)=> {
-  let filename = i.toString(16).padStart(64, "0") + ".json";
+  let filenameHex = i.toString(16).padStart(64, "0") + ".json";
+  let filenameDec = "" + i + ".json";
+
   let content = {
         "description": "Money Maker - " + i,
         "external_url": "https://forum.openzeppelin.com",
-        "image": "https://raw.githubusercontent.com/ximplarandy/CrazyNFTMeta/main/data/" + i + ".png",
+        "image": "https://raw.githubusercontent.com/ximplarandy/NuSkinMeta/main/data/" + i + ".png",
         "name": "Money Maker " + i
       }
-  fs.writeFileSync('data/' + filename, JSON.stringify(content))
-
-  const variant = i%2==0?'female':'male' // By default 'male' and 'female' supported
-  const image = await avatar.generate("ahaha" + i+'@example.com', variant)
-  // Now `image` contains sharp image pipeline http://sharp.pixelplumbing.com/en/stable/api-output/
-  // you can write it to file
-  await image
-    .png()
-    .toFile("data/" + i+ '.png')
+  fs.writeFileSync('data/' + filenameHex, JSON.stringify(content))
+  fs.writeFileSync('data/' + filenameDec, JSON.stringify(content))
 }
 for (let i = 0; i < 50; i++) {
   console.log("generating:" + i);
